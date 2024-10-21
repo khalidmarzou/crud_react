0ab577dbb865563fd60e64e6948d44a786a4b8cb
khalidmarzou <khalidmarzoug@gmail.com>
2024-10-21 10:38:38 +0100

    add selection in tr

---
+++ src/components/TR.jsx
@@ -0,0 +1,12 @@
+export default function TR() {
+  return (
+    <tr class="bg-white border-b hover:bg-gray-100 cursor-pointer focus:bg-red-50" tabIndex={1}>
+      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+        Apple MacBook Pro 17"
+      </th>
+      <td class="px-6 py-4">Silver</td>
+      <td class="px-6 py-4">Laptop</td>
+      <td class="px-6 py-4">$2999</td>
+    </tr>
+  );
+}
